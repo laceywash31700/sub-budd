8043fb56ea3cab57e538a86997c6e24309924172
Lacey Washington <lacey.m.washington@gmail.com>
2025-04-08 17:03:57 -0500

    added https server protections

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import https from "https";
+import fs from "fs";
 import express from "express";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
@@ -9,13 +11,12 @@ import workflowRouter from "./routes/workflow.routes.js";
 import { arcjetMW } from "./middleware/arcjet.middleware.js";
 import { PORT } from "./config/env.js";
 
-
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(arcjetMW)
+app.use(arcjetMW);
 
 // Routes
 app.use("/api/v1/auth", authRouter);
@@ -23,19 +24,28 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/workflows", workflowRouter);
 
+// Error handling
+app.use(errorMiddleware);
 
-
-// error handling
-app.use(errorMiddleware)
-
-app.get('/', (req, res) => {
- res.send("welcome to Sub-Bud");
+app.get("/", (req, res) => {
+  res.send("Welcome to Sub-Bud");
 });
 
-// listener
-app.listen(PORT, async () => {
-  console.log(`Sub Bud API is running on http://localhost:${PORT}`);
-  await  connectToDatabase();
+// HTTPS Server Configuration
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/sub-budd.zapto.org/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/sub-budd.zapto.org/cert.pem"),
+  ca: fs.readFileSync("/etc/letsencrypt/live/sub-budd.zapto.org/chain.pem"),
+};
+
+// HTTPS Listener
+https.createServer(options, app).listen(443, async () => {
+  console.log("Sub-Bud API is running on https://sub-budd.zapto.org");
 });
 
-export default app;
+// Optional: Redirect HTTP to HTTPS
+import http from "http";
+http.createServer((req, res) => {
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+}).listen(80);
